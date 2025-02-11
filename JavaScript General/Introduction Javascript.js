c27942b665fdbc1e8a272fcacd052257a5cae5e7
Krists Krievins <krists.krievins@gmail.com>
2025-02-11 15:34:32 +0100

    Ironhack Training Repository - Javascript

---
+++ JavaScript General/Introduction Javascript.js
@@ -0,0 +1,6 @@
+// This is a comment for JavaScript
+
+/* This is a multi-line comment
+   for JavaScript */
+
+console.log("My name is Bob, and I'm writing JavaScript");
---
+++ JavaScript General/JS Variables.js
@@ -0,0 +1,6 @@
+let = "hello"; // <== error, you can't name variable "let"
+
+console.log(let); // <== error, you can't name variable "let"
+{
+  TextDecoderStream: "hello";
+}
