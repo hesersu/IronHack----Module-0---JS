f20dc2e0e9c790fc72c79f381abe3dc76fbab293
Krists Krievins <krists.krievins@gmail.com>
2025-02-11 16:35:21 +0100

    Completed more Tasks

---
+++ Loops/5. Find the Bug.js
@@ -0,0 +1,17 @@
+// let bootcamp = "IRONHACK";
+
+// // Your code here:
+
+// for (let i = 0; i < bootcamp.length; ++i) {
+//   const char = bootcamp[i];
+//   console.log(char);
+// }
+
+let word = "TRAMS";
+
+// Your code here:
+
+for (let i = word.length - 1; i >= 0; --i) {
+  const char = word[i];
+  console.log(char);
+}
--- Loops/JS Loops 1.js
+++ Loops/JS Loops 1.js
@@ -1,1 +1,6 @@
-// Nested Loops
+let counter = 20;
+
+while (counter >= 0) {
+  console.log(counter);
+  counter--;
+}
